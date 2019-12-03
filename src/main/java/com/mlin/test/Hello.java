f57d2a2d5760f3ef5d0e74150b1a1c3b802f1677
aa_glb <aa_glb@qq.com>
2019-12-03 08:50:05 +0800

    this is my commit first

---
+++ src/main/java/com/mlin/test/Hello.java
@@ -0,0 +1,14 @@
+package com.mlin.test;
+
+public class Hello {
+
+
+    public void main(){
+
+    }
+
+
+   public void add(){
+       System.out.println("hello");
+   }
+}
---
+++ src/main/java/com/mlin/test/Word.java
@@ -0,0 +1,8 @@
+package com.mlin.test;
+
+public class Word {
+
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
